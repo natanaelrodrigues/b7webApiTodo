4675ef572e3489ad883a9c5086c4f9cc8a8e54a6
Natanael R. Rodrigues <natanaelrodrigues@gmail.com>
2021-01-05 20:56:33 -0300

    Criação de autenticação Sanctum

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+
 use App\Models\User;
 
 
@@ -34,6 +39,26 @@ class AuthController extends Controller
         $user->save();
 
         // falta logar no  usuário criado
+
+        return $array;
+    }
+
+    public function login(Request $request){
+        $array = ['error' => ''];
+
+        $creds = $request->only('email', 'password');
+
+        if(Auth::attempt($creds)) {
+
+            $user = User::where('email',$creds['email'])->first();
+            $item = time().rand(0,9999);
+            $token = $user->createToken($item)->plainTextToken;
+
+            $array['token'] = $token;
+
+        }else{
+            $array['error'] = 'E-mail e/ou senha incorretos';
+        }
 
         return $array;
     }
--- routes/api.php
+++ routes/api.php
@@ -12,11 +12,15 @@ Route::get('/ping',function(){
     ];
 });
 
-Route::post('/todo',[ApiController::class,'createTodo']);
+Route::get('/unauthenticated', function(){
+    return ['error' => 'Usuário não logado.'];
+})->name('login');
+
+Route::middleware('auth:sanctum')->post('/todo',[ApiController::class,'createTodo']);
 Route::get('/todos',[ApiController::class,'readAllTodos']);
 Route::get('/todo/{id}',[ApiController::class,'readTodo']);
 Route::put('/todo/{id}',[ApiController::class,'updateTodo']);
 Route::delete('/todo/{id}',[ApiController::class,'deleteTodo']);
 
 Route::post('/user',[AuthController::class,'create']);
-
+Route::post('/auth',[AuthController::class, 'login']);

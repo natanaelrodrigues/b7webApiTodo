e20ac411db15d5f77036b2cce53125fcf5981ce1
Natanael  R. Rodigues <natanaelrodrigues@gmail.com>
2021-01-05 15:11:05 -0300

    Exclusão do script não usado.

--- database/scripts/Todos.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE todos (
-  id serial NOT NULL PRIMARY KEY,
-	name varchar(100),
-	email varchar(200) not null,
-	password varchar(100) not null,
-	remember_token varchar(100) null,
-	active integer default 1
-);

2b3592d740b7ba21fe3b4b7849b61ceb707e00b2
Natanael  R. Rodigues <natanaelrodrigues@gmail.com>
2021-01-05 15:25:01 -0300

    Criação e configuração de rotas.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function createTodo(Request $request){
+
+    }
+
+    public function readAllTodos(){
+
+    }
+
+    public function readTodo($id){
+
+    }
+
+    public function updateTodo(Request $request, $id){
+
+    }
+
+    public function deleteTodo($id){
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+use App\Http\Controllers\ApiController;
 
 Route::get('/ping',function(){
     return [
         'pong'=> true
     ];
 });
+
+Route::post('/todo',[ApiController::class,'createTodo']);
+Route::get('/todos',[ApiController::class,'readAllTodos']);
+Route::get('/todo/{id}',[ApiController::class,'readTodo']);
+Route::put('/todo/{id}',[ApiController::class,'updateTodo']);
+Route::delete('/todo/{id}',[ApiController::class,'deleteTodo']);
+
